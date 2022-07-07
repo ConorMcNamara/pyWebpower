b00376f188f82b4c0497979587bbf8bbfb538fc8
Conor McNamara <conor.s.mcnamara@gmail.com>
2022-07-07 14:28:38 -0700

    Update utils.py

--- webpower/utils.py
+++ webpower/utils.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 def nuniroot(f, low_val: float = 0, high_val: float = 1, max_length: int = 100) -> float:
-    """
+    """Calculates the root of our function f given low_val and high_val
 
     Parameters
     ----------
@@ -24,7 +24,9 @@ def nuniroot(f, low_val: float = 0, high_val: float = 1, max_length: int = 100) 
     x = np.linspace(low_val, high_val, max_length)
     f_output = np.array([f(x_i) for x_i in x])
     if min(f_output) * max(f_output) > 0:
-        raise ValueError("The specified parameters do not yield valid results. Please try to supply a different interval, e.g., using interval=c(0,1), for your parameter.")
+        raise ValueError(
+            "The specified parameters do not yield valid results. Please try to supply a different interval, e.g., "
+            "using interval=[0, 1], for your parameter.") 
     else:
         low = max(f_output[f_output < 0])
         high = min(f_output[f_output > 0])
